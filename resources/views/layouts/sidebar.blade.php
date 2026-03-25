c51f1157f7c1dce183240b36e10b3b78a011a188
Guma Desarrollo <ANALISTA.GUMA@GMAIL.COM>
2026-03-25 11:45:19 -0600

    ✅ [ADD] Update sidebar
    Update sidebar layout and navigation labels for improved clarity

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,45 +20,63 @@
             <ul class="navbar-nav flex-column mb-3" id="navbarVerticalNav">
                 <li class="nav-item">
                     <a class="nav-link dropdown-indicator" href="#dashboard" role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="dashboard">
-                        <div class="d-flex align-items-center"><span class="nav-link-icon"><span class="fas fa-chart-pie"></span></span><span class="nav-link-text ps-1">Dashboard</span>
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-chart-pie"></span>
+                            </span>
+                            <span class="nav-link-text ps-1">Dashboard</span>
                         </div>
                     </a>
                     <ul class="nav collapse show" id="dashboard">
                         <li class="nav-item">
                             <a class="nav-link active" href="{{ route('dashboard')}}" aria-expanded="false">
-                                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Dashboard</span>
+                                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">GENERAL</span>
                                 </div>
                             </a>
-                            <a class="nav-link dropdown-indicator" href="#documents" role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="forms">
-                                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Documentos</span>
+                        </li>
+                        <li class="nav-item">
+                            <div class="row navbar-vertical-label-wrapper mt-3 mb-2">
+                                <div class="col-auto navbar-vertical-label">MODULOS</div>
+                                <div class="col ps-0">
+                                    <hr class="mb-0 navbar-vertical-divider" />
                                 </div>
-                            </a>
-                            <ul class="nav collapse false" id="documents">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('new-doc') }}" aria-expanded="false">
-                                        <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Nuevo</span>
-                                        </div>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('list-doc') }}" aria-expanded="false">
-                                        <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Lista</span>
-                                        </div>
-                                    </a>
-                                </li>
-                            </ul>
-                            
+                            </div>
                         </li>
                     
+                    <a class="nav-link dropdown-indicator" href="#icons" role="button" data-bs-toggle="collapse" aria-expanded="true" aria-controls="icons">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-file-alt"></span>
+                            </span>
+                            <span class="nav-link-text ps-1">DIGITALIZACION</span>
+                        </div>
+                    </a>
+                    <ul class="nav collapse show" id="icons">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('users.list') }}" aria-expanded="false">
-                                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">Usuarios</span>
+                            <a class="nav-link" href="{{ route('new-doc') }}" aria-expanded="false">
+                                <div class="d-flex align-items-center">
+                                    <span class="nav-link-text ps-1">AGREGAR</span>
                                 </div>
                             </a>
-                            <!-- more inner pages-->
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('list-doc') }}" aria-expanded="false">
+                                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">DEPART. LEGAL</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('list-doc') }}" aria-expanded="false">
+                                <div class="d-flex align-items-center"><span class="nav-link-text ps-1">DEPART. REGENCIA</span>
+                                </div>
+                            </a>
                         </li>
                         
                     </ul>
+                    <a class="nav-link" href="{{ route('users.list') }}" role="button" aria-expanded="false">
+                        <div class="d-flex align-items-center"><span class="nav-link-icon"><span class="fas fa-users"></span></span><span class="nav-link-text ps-1">Usuarios</span>
+                        </div>
+                    </a>
                 </li>
             </ul>
         </div>
